eb6ea318b3d67f93b9ff680bee5a4b7e88e35588
nkkf <nelsonkenmochi@gmail.com>
2020-04-19 09:57:35 -0400

    First commit

---
+++ index.js
@@ -0,0 +1,38 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+io.on('connection', function (socket) {
+
+    socket.on('JoinRoom', (arg1) => {
+
+        // É admin
+        if (arg1) {
+            for (let i = 0; true; i++) {
+                if (!roomsList.rooms["room-" + i]) {
+                    socket.join("room-" + i);
+                    socket.emit('connectToRoom', i);
+                    break;
+                }
+            }
+        } else {
+            for (let i = 0; true && i < 100; i++) {
+                if (roomsList.rooms["room-" + i] && roomsList.rooms["room-" + i].length == 1) {
+                    socket.join("room-" + i);
+                    socket.emit('connectToRoom', i);
+                    break;
+                }
+            }
+        }
+    });
+
+    socket.on('messageToServer', (author, roomID, msg) => {
+        socket.to("room-" + roomID).emit('message', author, msg)
+    });
+
+    const roomsList = io.nsps['/'].adapter;
+});
+
+
+
+http.listen(process.env.PORT || 3000);
